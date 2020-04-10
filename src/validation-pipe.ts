87cd7b4ba0757f5fb6d560026c22a9e4069c825d
Junior Miranda <jr.miranda@outlook.com>
2020-04-10 08:37:23 -0300

    fix: correct return of validation 422

--- src/validation-pipe.ts
+++ src/validation-pipe.ts
@@ -9,7 +9,8 @@ export class ValidationPipe422 extends ValidationPipe {
       return await super.transform(value, metadata);
     } catch (e) {
       if (e instanceof BadRequestException) {
-        throw new UnprocessableEntityException(e.message);
+        const err: any = e.getResponse();
+        throw new UnprocessableEntityException(err?.message);
       }
     }
   }

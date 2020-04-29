22f77575b5e48dc2b5cb3e4d0ef02ed49a4b0d8e
Junior Miranda <jr.miranda@outlook.com>
2020-04-29 18:08:20 -0300

    feat: add method of paginations

---
+++ src/modules/shared/types/paginate-method.type.ts
@@ -0,0 +1,10 @@
+import {
+  FilterQuery, PaginateOptions, PaginateResult,
+} from 'mongoose';
+
+
+export type PaginateMethod<T> = (
+  query?: FilterQuery<T>,
+  options?: PaginateOptions,
+  callback?: (err: any, result: PaginateResult<T>) => void,
+) => Promise<PaginateResult<T>>;
